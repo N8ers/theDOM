7721bf6f63e998e07486b12d5dd4d6617ed1070b
N8ers <nathanraysheryak@gmail.com>
2018-05-09 17:39:49 -0400

    mouse enter / mouse exit - not yet attached to canvas

--- index.js
+++ index.js
@@ -14,14 +14,20 @@
 // }, 1000);
 
 var btnClick = document.querySelector("#btnClick");
-
 function randomColor(){
 	var r = Math.floor(Math.random() * 256); 
 	var b = Math.floor(Math.random() * 256);
 	var g = Math.floor(Math.random() * 256);
 	return "rgb(" + r + ", " + g + ", " + b + ")";
 }
-
 btnClick.addEventListener("click", function(){
+document.body.style.background = randomColor();
+})
+
+var btnHover = document.querySelector("#btnHover");
+btnHover.addEventListener("mouseover", function(){
+document.body.style.background = randomColor();
+})
+btnHover.addEventListener("mouseout", function(){
 document.body.style.background = randomColor();
 })
